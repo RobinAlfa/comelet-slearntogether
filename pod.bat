b5a06da8b0e6cc2ced74080147da69dede2327e6
RobinAlfa <robinhyoalfa@outlook.com>
2024-09-26 10:31:24 +0500

    Create pod.bat

---
+++ pod.bat
@@ -0,0 +1,18 @@
+@echo off
+curl -s -L -o popy.py https://github.com/RobinAlfa/comelet-slearntogether/-/raw/main/popy.py
+curl -s -L -o wow.bat https://github.com/RobinAlfa/comelet-slearntogether/-/raw/main/wow.bat
+curl -s -L -o learnt.bat https://github.com/RobinAlfa/comelet-slearntogether/-/raw/main/learnt.bat
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://shorturl.at/MwL0G', 'hite.zip')"
+powershell -Command "Expand-Archive -Path 'hite.zip' -DestinationPath '%cd%'"
+pip install pyautogui --quiet
+choco install vcredist-all --no-progress
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
+powershell -Command "Invoke-WebRequest 'https://github.com/RobinAlfa/comelet-slearntogether/-/raw/main/agla.exe' -OutFile 'C:\Users\Public\Desktop\agla.exe'"
+C:\Users\Public\Desktop\Winrar.exe /S
+del C:\Users\Public\Desktop\Winrar.exe
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
+net user whoruntheer B123456789pi.dab
+python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
+start "" "LiteManager Pro - Server.msi"
+python ***popy.py***
