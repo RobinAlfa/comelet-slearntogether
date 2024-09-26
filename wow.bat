b99e799a78c7a1d938a9bd4d1d95b6e8d61223cc
RobinAlfa <robinhyoalfa@outlook.com>
2024-09-26 10:29:24 +0500

    Create wow.bat

---
+++ wow.bat
@@ -0,0 +1,8 @@
+@echo off
+start "" /MAX "C:\Users\Public\Desktop\agla"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+echo learner naam : whor*******
+echo learner chaabi : B************dab
