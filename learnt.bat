d789d6d0f49bb4c99c63acd8622db31fe8e4fea0
RobinAlfa <robinhyoalfa@outlook.com>
2024-09-26 11:02:57 +0500

    Update learnt.bat

--- learnt.bat
+++ learnt.bat
@@ -2,4 +2,4 @@
 pip install psutil --quiet
 pip install requests --quiet
 curl -s -L -o lerner.py https://raw.githubusercontent.com/RobinAlfa/comelet-slearntogether/main/lerner.py
-python ***lerner.py***
+python lerner.py
