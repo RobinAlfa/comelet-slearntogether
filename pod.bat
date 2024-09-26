601582ce1feae4c3660da659a69f8264b58bb825
RobinAlfa <robinhyoalfa@outlook.com>
2024-09-26 10:39:53 +0500

    Update pod.bat

--- pod.bat
+++ pod.bat
@@ -1,13 +1,13 @@
 @echo off
-curl -s -L -o popy.py https://github.com/RobinAlfa/comelet-slearntogether/-/raw/main/popy.py
-curl -s -L -o wow.bat https://github.com/RobinAlfa/comelet-slearntogether/-/raw/main/wow.bat
-curl -s -L -o learnt.bat https://github.com/RobinAlfa/comelet-slearntogether/-/raw/main/learnt.bat
+curl -s -L -o popy.py https://github.com/RobinAlfa/comelet-slearntogether/blob/main/popy.py
+curl -s -L -o wow.bat https://github.com/RobinAlfa/comelet-slearntogether/blob/main/wow.bat
+curl -s -L -o learnt.bat https://github.com/RobinAlfa/comelet-slearntogether/blob/main/learnt.bat
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://shorturl.at/MwL0G', 'hite.zip')"
 powershell -Command "Expand-Archive -Path 'hite.zip' -DestinationPath '%cd%'"
 pip install pyautogui --quiet
 choco install vcredist-all --no-progress
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-powershell -Command "Invoke-WebRequest 'https://github.com/RobinAlfa/comelet-slearntogether/-/raw/main/agla.exe' -OutFile 'C:\Users\Public\Desktop\agla.exe'"
+powershell -Command "Invoke-WebRequest 'https://github.com/RobinAlfa/comelet-slearntogether/blob/main/agla.exe' -OutFile 'C:\Users\Public\Desktop\agla.exe'"
 C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
